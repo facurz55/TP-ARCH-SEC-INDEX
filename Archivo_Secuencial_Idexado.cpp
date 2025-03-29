5d7b4674cc411563bc4a30586a82e60c6b902b12
facurz55 <facurz55@gmail.com>
2025-03-28 21:08:50 -0300

    update

--- Archivo_Secuencial_Idexado.cpp
+++ Archivo_Secuencial_Idexado.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+struct Area_datos {
+	int clave;
+	string dato;
+	int dir;
+};
+
+struct Area_indices {
+	int clave;
+	int dir;
+};
+
+void inicializar_areas(vector<Area_datos*>& datos, vector<Area_indices*>& indices, int OMAX, int cant_bloques) {
+	for (int i = 0; i < OMAX; i++) {
+		datos[i] = new Area_datos;  // Asignar memoria
+		datos[i]->clave = 0;
+		datos[i]->dir = 0;
+	}
+	
+	for (int i = 0; i < cant_bloques; i++) {
+		indices[i] = new Area_indices;  // Asignar memoria
+		indices[i]->dir = 0;
+		indices[i]->clave = 0;
+	}
+}
+
+int main() {
+	int n = 4;
+	int PMAX = 12;
+	int OVER = 12;
+	int OMAX = 20;
+	int cant_bloques = PMAX / n;
+	
+	vector<Area_datos*> datos(OMAX);
+	vector<Area_indices*> indices(cant_bloques);
+	
+	// Inicializar áreas antes de usarlas
+	inicializar_areas(datos, indices, OMAX, cant_bloques);
+	
+	for (const auto& p : datos) {
+		cout << p->clave << endl;
+	}
+	
+	for (auto& p : datos) delete p;
+	for (auto& p : indices) delete p;
 	
 	return 0;
 }
